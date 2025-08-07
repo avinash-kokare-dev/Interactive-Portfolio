f16dc075399a35874ed8662bcb38de91d0249025
AviKokare <avinas.kokare@fitcircle.in>
2025-08-07 20:35:37 +0530

    fixed experience calculation

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,18 +1,21 @@
 export const getYearOfExperience = () => {
     const joiningYear = 2021;
-    const joiningMonth = 6;
+    const joiningMonth = 5;
     const date = new Date();
     
     let yearOfExperience;
     let monthOfExperience;
 
-    if(date.getMonth() + joiningMonth < 12){
-        monthOfExperience = date.getMonth() - joiningMonth < 0 ? date.getMonth() - joiningMonth + 12 : date.getMonth() - joiningMonth;
+    
+    if(date.getMonth() === joiningMonth){
+        yearOfExperience = date.getFullYear() - joiningYear;
+        return `${yearOfExperience}`;
+    }else if(date.getMonth() > joiningMonth){
+        yearOfExperience = date.getFullYear() - joiningYear;
+        monthOfExperience = date.getMonth() - joiningMonth;
+    }else{
         yearOfExperience = date.getFullYear() - joiningYear - 1;
-    }else if(date.getMonth() + joiningMonth >= 12){
-        monthOfExperience = joiningMonth - date.getMonth();
-        yearOfExperience = date.getFullYear() - joiningYear - 1;
+        monthOfExperience = (12 - joiningMonth) + date.getMonth();
     }
-
     return `${yearOfExperience}.${monthOfExperience}+`
 }
